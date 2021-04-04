f235e9f4c9b441787f4f0b5b27f972ea04c09837
Lucas Long <lucaslong96@gmail.com>
2021-04-04 13:35:32 -0400

    Added empty library array and Book class.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let library = [];
+
+class Book {
+    constructor(name, author, pages, read) {
+        this._name = name;
+        this._author = author;
+        this._pages = pages;
+        this._read = read;
+    }
+}
