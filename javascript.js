a87b0b7fb713ce3cfd80c406033a90fd28caf2b4
Lucas Long <lucaslong96@gmail.com>
2021-04-04 13:39:04 -0400

    Added pseudocode for addBookToLibrary() and removeBookFromLibrary()

--- javascript.js
+++ javascript.js
@@ -8,3 +8,16 @@ class Book {
         this._read = read;
     }
 }
+
+function addBookToLibrary() {
+    // take data from some sort of prompt and store in vars
+    // give error if all fields aren't filled out
+    // if all info looks correct, add to library 
+    // refresh library visuals
+}
+
+function removeBookFromLibrary() {
+    // find the index of a particular book in the library
+    // remove the book from the library
+    // refresh visuals 
+}
