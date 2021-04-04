1705452b5b8d62698e3a5854bcf0235728fe2f7a
Lucas Long <lucaslong96@gmail.com>
2021-04-04 14:25:51 -0400

    Added basic updateVisuals() function to display the books on the page.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-let library = [];
+const bookContainer = document.querySelector("#bookContainer");
+const library = [];
 
 class Book {
     constructor(name, author, pages, read) {
@@ -12,6 +13,26 @@ class Book {
 const book1 = new Book("egg", "eggerton", 314, true);
 const book2 = new Book("eggsalad", "eggies", 845, false);
 const book3 = new Book("bookname", "book writer", 123, true);
+
+library.push(book1, book2, book3);
+
+updateVisuals();
+
+function updateVisuals() {
+    library.forEach(book => {
+        const name = book._name;
+        const author = book._author;
+        const pages = book._pages;
+        const read = book._read;
+
+        let bookDiv = document.createElement("div");
+        bookDiv.classList = "bookDiv";
+        bookDiv.textContent = name + " was written by " + author + 
+        "." + " It has " + pages + " pages and read = " + read;
+
+        bookContainer.append(bookDiv);
+    });
+}
 
 function addBookToLibrary() {
     // take data from some sort of prompt and store in vars
